3af7a2f186ec1d2e67f1aa6519a16b820dca601a
MathysGoncalves <mathys.goncalves@efrei.net>
2022-03-24 15:21:36 +0100

    Changing Event and Citizen

--- src/main/scala/RunSceanario.scala
+++ src/main/scala/RunSceanario.scala
@@ -2,31 +2,18 @@ import com.github.tototoshi.csv._
 import faker._
 
 import java.io.File
-import java.util.Date
-
-case class Event(
-                  val citizen: String,
-                  val peacescore: Int,
-                  val latitude: Double,
-                  val longitude: Double,
-                  val wordHeard: String,
-                  val date: Date,
-                  val battery: Int,
-                  val temperature: Int,
-                  val droneId:String,
-                )
 
 object RunSceanario {
 
   val r = scala.util.Random
-
   def seq(p:Product) = p.productIterator.toList
 
-  def dataGen(rows:Int, drone_Id:String) = seq(java.util.UUID.randomUUID.toString,
-    3 + r.nextInt(( 97 - 3) + 1),
+  def dataGen(drone_Id:String) = seq(
+    (java.util.UUID.randomUUID.toString, 3 + r.nextInt(( 97 - 3) + 1)),
     Address.latitude,
     Address.longitude,
     Lorem.sentence(),
+    (java.util.UUID.randomUUID.toString, 3 + r.nextInt(( 97 - 3) + 1), (java.util.UUID.randomUUID.toString, 3 + r.nextInt(( 97 - 3) + 1))),
     java.time.LocalDate.now,
     15 + r.nextInt(( 99 - 15) + 1),
     10 + r.nextInt(( 30 - 10) + 1),
@@ -35,11 +22,10 @@ object RunSceanario {
 
   def main(args: Array[String]): Unit = {
 
-    val p1 = dataGen(2,"1")
-    val p2 = dataGen(2,"2")
     val f = new File("data/event.csv")
     val writer = CSVWriter.open(f)
-    writer.writeAll(List(p1, p2))
+    val randomReport = (0 to 500).map(x => dataGen(drone_Id = x.toString))
+    writer.writeAll(randomReport)
     writer.close()
   }
 }

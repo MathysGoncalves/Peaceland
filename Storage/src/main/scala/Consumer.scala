1d369efbfb7535c45fbb3f4e9426d12b2eff4207
MathysGoncalves <mathys.goncalves@efrei.net>
2022-04-07 11:15:11 +0200

    Modif on Producer

--- Storage/src/main/scala/Consumer.scala
+++ Storage/src/main/scala/Consumer.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.Row.empty.schema
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 import java.text.SimpleDateFormat
@@ -17,66 +18,26 @@ object Consumer {
       .master("local[*]")
       .getOrCreate()
 
-    spark.sparkContext.setLogLevel("ERROR")
-
-    import spark.implicits._
     val df = spark
       .readStream
-      .format("kafka")//"org.apache.spark.sql.kafka010.KafkaSourceProvider")
+      .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "report")
-      .option("startingOffsets", "earliest")
+      .option("startingOffsets","earliest")
       .load()
 
-    df.printSchema()
+    val report = df.selectExpr("CAST(value AS STRING)")
 
-    val fileName = "data/storage/"
-
-    /*
-    df.writeStream
-      .format("json")
-      .option("path", fileName)
-      .option("checkpointLocation", "data/checkpoint/")
-      .outputMode("append")
-      //.trigger(processingTime="10 seconds")
+    val query = report.writeStream
+      .trigger(Trigger.ProcessingTime("30 seconds"))
+      .option("header","false")
+      .option("delimiter","|")
+      .format("csv")
+      .option("checkpointLocation", "data/checkpoints")
+      .option("path", "data/storage")
       .start()
-    */
 
-
-    val stringDF = df.selectExpr("CAST(value AS STRING)")
-
-    val schema = new StructType()
-      .add("citizen", StringType)
-      .add("lat", StringType)
-      .add("long", StringType)
-      .add("word", StringType)
-      .add("people", StringType)
-      .add("date", StringType)
-      .add("battery", StringType)
-      .add("temperature", StringType)
-      .add("droneId", StringType)
-
-    val report = stringDF.select(from_json($"value", schema).as("r"))
-      .select($"r.citizen",
-        $"r.lat",
-        $"r.long",
-        $"r.word",
-        $"r.people",
-        $"r.date",
-        $"r.battery",
-        $"r.temperature",
-        $"r.droneID")
-
-    report.writeStream
-      .format("console")
-      //.option("path", fileName)
-      //.option("checkpointLocation", "data/checkpoint/")
-      .outputMode("append")
-      //.trigger(processingTime="10 seconds")
-      .start()
-      .awaitTermination()
-
-    spark.stop()
-
+    query.awaitTermination()
+    spark.close
   }
 }
